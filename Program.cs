3b62cfe2af1982ace151b44ba48c2a2dff31a905
criorre <chococharity17@gmail.com>
2025-12-31 21:15:54 -0600

    Implement 4d6 roll and output

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// this code produces "psuedorandom" numbers, meaning it simulates randomness via definite mathematics
+
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+Random d6 = new Random();
+
+int roll1 = d6.Next(1, 7); // minimum is included, maximum is not included. this outputs 1-6  (as 7 is omitted)
+int roll2 = d6.Next(1, 7);
+int roll3 = d6.Next(1, 7);
+int roll4 = d6.Next(1, 7);
+
+int fourd6 = roll1 + roll2 + roll3 + roll4;
+
+Console.WriteLine($"{roll1}, {roll2}, {roll3}, {roll4} = {fourd6}");
+
